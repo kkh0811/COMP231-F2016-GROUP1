597eefcafb7a97ae897eca600b32f1f8fa8b9d36
kkh0811 <gkim16@my.centennialcollege.ca>
2016-12-02 21:47:20 -0500

    added functions to secure the password
    Added functions to encrypt and decrypt the password

---
+++ PasswordApplication/Decryptor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace PasswordApplication
+{
+    class Decryptor
+    {
+        // AES_256 Decryption
+        public String AESDecrypt256(String InputText)
+        {
+            // the value of key will be changed in next iteration
+            string Key = "1234567890";
+
+            // Declare Rijndael class 
+            RijndaelManaged RinjindaelCipher = new RijndaelManaged();
+
+            byte[] EncryptedData = Convert.FromBase64String(InputText);
+            byte[] Salt = Encoding.ASCII.GetBytes(Key.Length.ToString());
+
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Key, Salt);
+
+            // Create Decryptor object
+            ICryptoTransform Decryptor = RinjindaelCipher.CreateDecryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+
+            MemoryStream memoryStream = new MemoryStream(EncryptedData);
+
+            // Create CryptoStream to read data
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, Decryptor, CryptoStreamMode.Read);
+
+            // Declare array to store data decrypted
+            byte[] PlainText = new byte[EncryptedData.Length];
+
+            // The process of encryption
+            int DecryptedCount = cryptoStream.Read(PlainText, 0, PlainText.Length);
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string DecryptedData = Encoding.Unicode.GetString(PlainText, 0, DecryptedCount);
+            return DecryptedData;
+        }
+    }
+}
---
+++ PasswordApplication/Encryptor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace PasswordApplication
+{
+    class Encryptor
+    {
+        // AES_256 Encryption
+        public String AESEncrypt256(String InputText)
+        {
+            // the value of key will be changed in next iteration
+            string Key = "1234567890";
+
+            RijndaelManaged RinjindaelCipher = new RijndaelManaged();
+
+            // Convert from String to Byte array
+            Byte[] PlainText = System.Text.Encoding.Unicode.GetBytes(InputText);
+
+            // Make a difficult key to prevent from dictionary attack
+            // Uses Salt
+            byte[] Salt = Encoding.ASCII.GetBytes(Key.Length.ToString());
+
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Key, Salt);
+
+            // Create a encryptor from the existing SecretKey bytes.
+            // Create encryptor object from SecretKey
+            // Secret Key ->  uses 32 bytes
+            // Initialization Vector -> uses 16 bytes
+            ICryptoTransform Encryptor = RinjindaelCipher.CreateEncryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Declare CryptoStream object to write data encrypted
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, Encryptor, CryptoStreamMode.Write);
+
+            cryptoStream.Write(PlainText, 0, PlainText.Length);
+            cryptoStream.FlushFinalBlock();
+            byte[] CipherBytes = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string EncryptedData = Convert.ToBase64String(CipherBytes);
+            return EncryptedData;
+        }
+    }
+}
