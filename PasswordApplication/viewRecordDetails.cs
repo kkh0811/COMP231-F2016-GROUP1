4f4937131b8ecc5751678cebe845976d71382aae
mekhaleganesh <mekhaleganesh@gmail.com>
2016-12-06 20:17:20 -0500

    -- Created Iview interface to view record -- added service name textbox  to the viewrecordform -- added showcheckbox to view the passwprd in text -- created viewRecordDetails class to view record using data model -- removed the getter and setter from viewrecord form -- added code to get the data from grid and passing it to viewrecorddetails class --GrabRecordID in viewrecordform store the record id --removed the previous collections in the combobox

---
+++ PasswordApplication/viewRecordDetails.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+using PasswordApplication.Interfaces;
+using PasswordApplication.Model;
+using PasswordApplication.AbstractClass;
+namespace PasswordApplication
+{
+    class viewRecordDetails : IView
+    {
+        public bool SelectEntity(AbDatabaseEntity Entity)
+        {
+            ViewRecordForm vrf = new ViewRecordForm();
+
+            //Initial connection maker to make a connention 
+            SQLServerConnMaker SQLconn = new SQLServerConnMaker();
+            SqlConnection conn = SQLconn.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            //Initial UserRecord entity
+            UserRecord userRecord = (UserRecord)Entity;
+            int recordID = userRecord.RecordID;
+            string userName = userRecord.UserName;
+            string userPassword = userRecord.UserPassword;
+            string serviceName = userRecord.ServiceName;
+            string note = userRecord.Note;
+            string categoryName = userRecord.CategoryName;
+
+
+
+            try
+            {
+                conn.Open();
+
+                vrf.GrabRecordID = userRecord.RecordID;
+                vrf.CategoryOptionComboBox.Items.Add(categoryName);
+                vrf.CategoryOptionComboBox.SelectedIndex = 0;
+                vrf.UserNameTextBox.Text = userName;
+                vrf.PasswordTextBox.Text = userPassword;
+                vrf.PasswordTextBox.PasswordChar = '*';
+                vrf.NoteTextBox.Text = note;
+                vrf.ServiceNameTextBox.Text = serviceName;
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            vrf.Show();
+
+            return true;
+        }
+    }
+}
